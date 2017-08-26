5b70f9c03f88301315a56fe982f67ebf59491da2
logikuma <logical.kumachan1004@gmail.com>
2017-08-27 00:25:37 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PiTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
